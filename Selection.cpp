883a05ab33a497211c8eb593c0ee5e7acfa1a132
dmswns0147@naver.com <dmswns0147@naver.com>
2023-04-25 16:15:29 +0900

    --

--- Selection.cpp
+++ Selection.cpp
@@ -4,27 +4,27 @@
 
 using namespace std;
 
-int quickSelect(vector<int> v, int k)
+template <typename T, typename = std::enable_if<std::is_arithmetic<T>::value>>
+int quickSelect(vector<T> v, int k)
 {
-    int pivot = v[0]; // 임의의 element를 pivot으로 설정
+    T pivot = v[0];
 
-    vector<int> A1, A2;
+    vector<T> smallGroupV, largeGroupV;
 
     for (auto x : v)
     {
-        // pivot을 기준으로 element partitioning
         if (x < pivot)
-            A1.push_back(x);
+            smallGroupV.push_back(x);
         else if (x > pivot)
-            A2.push_back(x);
+            largeGroupV.push_back(x);
     }
 
-    if (k <= A1.size()) // 찾고자 하는 answer가 A1에 있을 경우 subproblem 호출
-        return quickSelect(A1, k);
-    else if (k == A1.size() + 1) // 현재 problem의 answer가 pivot일 경우 반환
+    if (k <= smallGroupV.size())
+        return quickSelect(smallGroupV, k);
+    else if (k == smallGroupV.size() + 1)
         return pivot;
-    else // 찾고자 하는 answer가 A2에 있을 경우 subproblem 호출
-        return quickSelect(A2, k - (A1.size() + 1));
+    else
+        return quickSelect(largeGroupV, k - (smallGroupV.size() + 1));
 }
 
 int main(int argc, char const *argv[])
