5fb7abe864168469ba834da676138250f36205fb
dmswns0147@naver.com <dmswns0147@naver.com>
2023-05-01 09:55:47 +0900

    Point sort

--- ClosestPair.cpp
+++ ClosestPair.cpp
@@ -1,24 +1,59 @@
-#include <cstdlib>
+#include <algorithm>
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Point
-{
-private:
-    int x;
-    int y;
+class Point {
+   private:
+    int x, y;
 
-public:
-    int getX() { return x; }
-    double getLength(Point other)
-    {
+   public:
+    Point(int x, int y) : x(x), y(y) {}
+    int getX() const { return x; }
+    int getY() const { return y; }
+
+    double_t getLength(Point other) const {
         return sqrt(pow(abs(this->x - other.x), 2) + pow(abs(this->y - other.y), 2));
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Point& point) {
+        cout << "( " << point.x << " , " << point.y << " )";
+        return out;
     }
 };
 
-int main(int argc, char const *argv[])
-{
+double_t closest_pair(const vector<Point>& pv) {
+    if (pv.size() <= 3)
+        return brute_force(pv);
+}
+
+double_t brute_force(const vector<Point>& pv) {
     
+}
+
+int main(int argc, char const* argv[]) {
+    vector<Point> v;
+
+    v.push_back(Point(2, 2));
+    v.push_back(Point(5, 5));
+    v.push_back(Point(2, 4));
+    v.push_back(Point(7, 1));
+    v.push_back(Point(9, 3));
+    v.push_back(Point(5, 6));
+    v.push_back(Point(7, 4));
+
+    sort(v.begin(), v.end(), [](const Point& p1, const Point& p2) -> bool {
+        if (p1.getX() != p2.getX())
+            return p1.getX() < p2.getX();
+        else
+            return p1.getY() < p2.getY();
+    });
+
+    for (Point p : v)
+        cout << p << endl;
+
     return 0;
 }
--- Sort/MergeSort.cpp
+++ Sort/MergeSort.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
+
 #include "util.cpp"
 
 using namespace std;
 
 template <typename T, typename = enable_if<is_arithmetic<T>::value>>
-void merge(vector<T> &arr, int left, int middle, int right)
-{
-    vector<T> temp(right - left + 1); // 임시 배열 생성
+void merge(vector<T> &arr, int left, int middle, int right) {
+    vector<T> temp(right - left + 1);  // 임시 배열 생성
 
     int i = left;
     int j = middle + 1;
@@ -16,9 +16,9 @@ void merge(vector<T> &arr, int left, int middle, int right)
     int k = 0;
 
     // 작은 순서대로 배열에 삽입
-    // 한쪽이 먼저 전부 temp에 복사되면 다른 한쪽이 정렬되어야 하는 배열이 남아버림
-    while (i <= middle && j <= right)
-    {
+    // 한쪽이 먼저 전부 temp에 복사되면 다른 한쪽이 정렬되어야 하는 배열이
+    // 남아버림
+    while (i <= middle && j <= right) {
         if (arr[i] <= arr[j])
             temp[k++] = arr[i++];
         else
@@ -26,23 +26,18 @@ void merge(vector<T> &arr, int left, int middle, int right)
     }
 
     // 그래서 남은 데이터도 삽입
-    while (i <= middle)
-        temp[k++] = arr[i++];
-    while (j <= right)
-        temp[k++] = arr[j++];
+    while (i <= middle) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
 
     // 임시 배열의 데이터를 원래 배열으로 복사
-    for (int x = 0; x < k; x++)
-        arr[left + x] = temp[x];
+    for (int x = 0; x < k; x++) arr[left + x] = temp[x];
 }
 
 // T 타입을 산술 연산이 가능한 타입으로 제한
 template <typename T, typename = enable_if<is_arithmetic<T>::value>>
 // 매개변수 벡터를 수정해야 하므로 백터 참조자
-void mergeSort(vector<T> &arr, int left, int right)
-{
-    if (left < right)
-    {
+void mergeSort(vector<T> &arr, int left, int right) {
+    if (left < right) {
         int middle = (left + right) / 2;
 
         // 분할
@@ -54,20 +49,18 @@ void mergeSort(vector<T> &arr, int left, int right)
     }
 }
 
-int main(int argc, char const *argv[])
-{
-
+int main(int argc, char const *argv[]) {
     // 1번째부터 실제 전달한 인자
-    if (argc <= 0)
-    {
+    if (argc <= 0) {
         cout << "실행하는데 필요한 매개변수 수가 부족합니다." << endl;
         return -1;
     }
 
     const string targetPath = argv[1];
     vector<int> result = split(readFromFile(argv[1]), ' ');
-    mergeSort(result, 0, result.size() - 1); // 정렬 실행
-    const string resultPath = targetPath.substr(0, targetPath.find_last_of("/") + 1) + "result.txt";
+    mergeSort(result, 0, result.size() - 1);  // 정렬 실행
+    const string resultPath =
+        targetPath.substr(0, targetPath.find_last_of("/") + 1) + "result.txt";
     writeToFile(result, resultPath);
 
     return 0;
