73f439b8ca578c8c32ad79653e31ff5e284f0ec5
dmswns0147@naver.com <dmswns0147@naver.com>
2023-06-11 00:39:57 +0900

    omok

--- omok.c
+++ omok.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define LENGTH 18
+#define LENGTH 10
 
 enum STONE {
     NONE = 0,
@@ -21,16 +21,36 @@ bool isindexoutofboundsexception(int x, int y) {
         return false;
 }
 
-int getWeight(enum STONE board[LENGTH][LENGTH], int evex, int evey) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 2; j++) {
+bool bbbwPattern(enum STONE board[LENGTH][LENGTH], int evex, int evey) {
+    int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+    int dy[] = {0, -1, -1, -1, 0, 1, 1, 1};
+
+    enum STONE stone = board[evex][evey];
+
+    // 8방향검사
+    for (int direction = 0; direction < 8; ++direction) {
+        int count = 1;  // 흑돌 카운트
+
+        // 한 방향으로 계속 진행하며 동일한 돌이 연속되는지 검사
+        for (int i = 1; i <= 2; ++i) {
+            int nx = evex + dx[direction] * i;
+            int ny = evey + dy[direction] * i;
+
+            // 배열 범위를 벗어나거나, 검사하는 돌과 다른 돌이라면 중단
+            if (nx < 0 || nx >= LENGTH || ny < 0 || ny >= LENGTH || board[nx][ny] != stone)
+                break;
+            count++;
         }
+
+        // count가 3이고(흑독3개) 다른한쪽이 흰돌이면 트루
+        if (count == 3 && (board[evex - dx[direction]][evey - dy[direction]] != stone))
+            return true;
     }
 
-    return 1;
+    return false;
 }
 
-bool getWeight(enum STONE board[LENGTH][LENGTH], int evex, int evey) {
+bool bbbPattern(enum STONE board[LENGTH][LENGTH], int evex, int evey) {
     int dx[] = {1, 1, 0, -1, 1, -1, 0, -1};  // 가로, 세로, 대각선 검사
     int dy[] = {0, 1, 1, 1, -1, -1, -1, 0};
 
@@ -102,4 +122,130 @@ int turn(enum STONE board[LENGTH][LENGTH], int depth, int *a, int *b, int evex, 
             }
         }
     }
+}
+
+int main(int argc, char const *argv[]) {
+    enum STONE board[LENGTH][LENGTH] = {{
+                                            BLACK,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            BLACK,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            BLACK,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            WHITE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        },
+                                        {
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                            NONE,
+                                        }};
+
+    printf("%d", bbbwPattern(board, 2, 2));
+    return 0;
 }
