3193439ad954573e09a8ab54f9f2584ff4a3c17e
dmswns0147@naver.com <dmswns0147@naver.com>
2023-06-11 00:10:27 +0900

    omok

--- omok.c
+++ omok.c
@@ -22,7 +22,38 @@ bool isindexoutofboundsexception(int x, int y) {
 }
 
 int getWeight(enum STONE board[LENGTH][LENGTH], int evex, int evey) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+        }
+    }
+
     return 1;
+}
+
+bool getWeight(enum STONE board[LENGTH][LENGTH], int evex, int evey) {
+    int dx[] = {1, 1, 0, -1, 1, -1, 0, -1};  // 가로, 세로, 대각선 검사
+    int dy[] = {0, 1, 1, 1, -1, -1, -1, 0};
+
+    enum STONE stone = BLACK;  // 흑돌
+    for (int d = 0; d < 8; ++d) {
+        int count = 0;
+        for (int i = 1; i <= 3; ++i) {
+            int nx = evex + dx[d] * i;
+            int ny = evey + dy[d] * i;
+            if (nx < 0 || nx >= LENGTH || ny < 0 || ny >= LENGTH)
+                break;
+            if (board[nx][ny] == stone) {
+                if (i == 2 && (nx == evex || ny == evey))
+                    break;  // 직전 돌의 위치인 경우 패턴 미완성
+                ++count;
+            } else {
+                break;
+            }
+        }
+        if (count == 3)
+            return true;
+    }
+    return false;
 }
 
 // 알파가 큰거 베타가 작은거
@@ -68,7 +99,6 @@ int turn(enum STONE board[LENGTH][LENGTH], int depth, int *a, int *b, int evex, 
         for (int x = 0; x < LENGTH; x++)  // for each child of node
         {
             for (int y = 0; y < LENGTH; y++) {
-                
             }
         }
     }
